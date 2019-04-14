a43a8397f66473498b57ab5a0652fb184330a65a
Subat Hayrat <subat0106@gmail.com>
2019-04-14 22:27:09 +0000

    change mongo db line

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ var  indexRoutes      = require("./routes/index"),
      //feeRoutes        = require("./routes/fee");
     
 //DB Connection
-//app.locals.moment = require("moment");
+app.locals.moment = require("moment");
 mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
-mongoose.connect("mongodb://localhost:27017/yorkland_admin", { useNewUrlParser: true });
+//mongoose.connect("mongodb://localhost:27017/yorkland_admin", { useNewUrlParser: true });
 mongoose.set('useCreateIndex', true);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine","ejs");
